220b27a50dfddeb9f9d19dc53b5493fdfe3eb9c1
Matthew Williams <FatBat@Matthews-iMac.local>
2016-07-30 17:18:23 +0200

    began work on fprime

---
+++ 4/fprime/fprime.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void	fprime(int num)
+{
+	int		cnt;
+	int		fcnt;
+	int		pcnt;
+
+	cnt = 1;
+	fcnt = 0;
+	//pcnt = 0;
+	while (cnt <= num)
+	{
+		pcnt = 0;
+		if ((num % cnt) == 0)
+		{
+			fcnt = 1;
+			while (fcnt <= cnt)
+			{
+				if ((cnt % fcnt) == 0)
+				{
+					pcnt++;
+				}
+				fcnt++;
+			}
+			if (pcnt == 2)
+			{
+				printf("%i", cnt);
+				printf(" ");
+			}
+		}
+		cnt++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int		num;
+
+	if (argc != 2) //argc < 2
+	{
+		//write(1, "\n", 1);
+		printf("\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("\n");
+		return (1);
+	}
+	fprime(num);
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ExPrac
+
+##Level 1:
+##Level 2:
+##Level 3:
+##Level 4:
+	fprime:
+		works, but doesn't output correctly, ie:
+			./a.out 225225
+				outputs:
+			3 5 7 11 13
+				should output:
+			3 3 5 5 7 11 13
+				also needs '*' char
+			3 * 3 * 5 * 5 * 7 * 11 * 13
+
+##Level 5:
+	brainfuck:
+		works
+
